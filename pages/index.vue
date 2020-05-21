ccf0dd76823e6fc06250eff619fbde8911514ad5
kmatsui058 <kmatsui058@gmail.com>
2020-05-21 14:38:36 +0900

    tech 追加

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@
         <tr>
           <th> Name </th>
           <th> Stating Law </th>
+          <th> Stating Tech </th>
         </tr>
       </thead>
       <tbody>
@@ -15,6 +16,7 @@
           <tr v-if="typeof item.Name === 'string'" :id="item.Name" :key="item.Name">
             <td>{{ getName(item.Name.toString()) }}</td>
             <td>{{ getLaw(item.aeStartingLaw) }}</td>
+            <td>{{ getTech(item.aeStartingTech) }}</td>
           </tr>
         </template>
       </tbody>
@@ -28,6 +30,7 @@ import Logo from '~/components/Logo.vue'
 import nation from '~/assets/data/xml/nation'
 import textNation from '~/assets/data/xml/text-nation'
 import law from '~/assets/data/xml/law'
+import tech from '~/assets/data/xml/tech'
 // import name from '~/assets/data/xml/name'
 @Component({
   components: {
@@ -38,6 +41,7 @@ export default class Index extends Vue {
   nations = nation.Root.Entry
   textNations = textNation.Root.Entry
   law = law.Root.Entry
+  tech = tech.Root.Entry
   getName (key: string): string {
     const test = this.textNations.find(el => el.zType === key)
     return test ? test.English.toString().split('~')[0] : ''
@@ -47,6 +51,19 @@ export default class Index extends Vue {
     const key = obj.Pair ? obj.Pair.zValue : ''
     const test = this.law.find(el => el.zType === key)
     return test ? test.zName.toString() : ''
+  }
+
+  getTech (obj: XmlNation.AeStartingTech): string {
+    const result: string[] = []
+    const keys = obj.zValue ? obj.zValue : []
+    keys.forEach((key) => {
+      console.log(key)
+      const test = this.tech.find(el => el.zType === key)
+      console.log(test)
+      if (test && typeof test.zName === 'string') { result.push(test.zName) }
+    })
+
+    return result.join(', ')
   }
 }
 </script>
