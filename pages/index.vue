121ba18151e45beba18cd5d92e889d20853fdc03
kmatsui058 <kmatsui058@gmail.com>
2021-05-05 12:58:46 +0900

    text-infoをバンドルしてみる

--- pages/index.vue
+++ pages/index.vue
@@ -25,11 +25,13 @@
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import { DefaultApi } from '~/api'
 import Logo from '~/components/Logo.vue'
 import nation from '~/assets/data/xml/nation'
 import textNation from '~/assets/data/xml/text-nation'
 import law from '~/assets/data/xml/law'
 import tech from '~/assets/data/xml/tech'
+import textInfo from '~/assets/data/xml/text-infos'
 // import name from '~/assets/data/xml/name'
 @Component({
   components: {
@@ -41,8 +43,15 @@ export default class Index extends Vue {
   textNations = textNation.Root.Entry
   law = law.Root.Entry
   tech = tech.Root.Entry
+  textInfo = textInfo.Root.Entry
+
   getName (key: string): string {
     const test = this.textNations.find(el => el.zType === key)
+    return test ? test.English.toString().split('~')[0] : ''
+  }
+
+  getTechName (key: string): string {
+    const test = this.textInfo.find(el => el.zType === key)
     return test ? test.English.toString().split('~')[0] : ''
   }
 
@@ -51,10 +60,16 @@ export default class Index extends Vue {
     const keys = obj.zValue ? obj.zValue : []
     keys.forEach((key) => {
       const test = this.tech.find(el => el.zType === key)
-      if (test && typeof test.Name === 'string') { result.push(test.Name) }
+      if (test && typeof test.Name === 'string') { result.push(this.getTechName(test.Name)) }
     })
 
     return result.join(', ')
+  }
+
+  created () {
+    new DefaultApi({
+      basePath: 'http://localhost:8080/api'
+    }).authGoogleRedirectGet()
   }
 }
 </script>
