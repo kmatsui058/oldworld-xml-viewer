f1fce7013083e55043c3f3728bb7fd0da395faf8
kmatsui058 <kmatsui058@gmail.com>
2021-05-05 09:40:51 +0900

    クライアントシークレットハッシュ追加

--- service/app/Providers/AuthServiceProvider.php
+++ service/app/Providers/AuthServiceProvider.php
@@ -28,6 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
         if (! $this->app->routesAreCached()) {
             Passport::routes();
+            Passport::hashClientSecrets();
         }
         //
     }
