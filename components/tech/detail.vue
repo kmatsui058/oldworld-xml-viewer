29fc1e68efbacee51fe1eda8619f4b910342d326
kmatsui058 <kmatsui058@gmail.com>
2022-04-16 14:58:14 +0900

    コンポーネントの不足対応

---
+++ components/tech/detail.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="content">
+    <h3>
+      {{ title }}
+    </h3>
+  </div>
+</template>
+<script lang="ts">
+import { computed, defineComponent } from '@nuxtjs/composition-api'
+
+import Tech from '~/classes/Tech'
+export default defineComponent({
+  name: 'TechDetail',
+  props: {
+    zType: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const TechItem = computed(() => {
+      return new Tech(props.zType)
+    })
+
+    const title = computed(() => {
+      const items = TechItem.value.name?.split('~')[0]
+      return items
+    })
+
+    return { TechItem, title }
+  },
+})
+</script>
+<style lang="scss" scoped>
+.effect-city {
+  margin: 0;
+  list-style: none;
+}
+.icon {
+  height: 1em;
+  width: 1em;
+  vertical-align: text-bottom;
+}
+</style>
+
